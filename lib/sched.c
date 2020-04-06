bb1571c8839b69547e9021516c3c1df7e4b60cbb
tongtao <605151707@qq.com>
2020-04-06 22:16:53 +0000

    v1.4

--- lib/sched.c
+++ lib/sched.c
@@ -29,4 +29,23 @@ void sched_yield(void)
      *  functions or macros below may be used (not all):
      *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
      */
+
+    static struct Env *e;
+    count--;
+    if(count <= 0){
+    	do {
+        	if(LIST_EMPTY(&env_sched_list[point]))
+        	{
+            	point = 1 - point;
+       		}
+        	e = LIST_FIRST(&env_sched_list[point]);
+			if (e != NULL) {
+        		LIST_REMOVE(e, env_sched_link);
+        		LIST_INSERT_HEAD(&env_sched_list[1-point], e, env_sched_link);
+        		count = e->env_pri;
+			}
+    	}
+		while(e == NULL || e->env_status != ENV_RUNNABLE);
+	}
+    env_run(e);
 }
