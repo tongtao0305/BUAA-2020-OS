99f4fd6f084a6d82b518f0429f8add8b2f7c5c2b
tongtao <605151707@qq.com>
2020-04-30 19:57:53 +0000

    v2.1.0

--- lib/sched.c
+++ lib/sched.c
@@ -16,6 +16,7 @@ void sched_yield(void)
 {
     static int count = 0; // remaining time slices of current env
     static int point = 0; // current env_sched_list index
+    static struct Env *e = NULL;
     
     /*  hint:
      *  1. if (count==0), insert `e` into `env_sched_list[1-point]`
@@ -30,19 +31,23 @@ void sched_yield(void)
      *  LIST_INSERT_TAIL, LIST_REMOVE, LIST_FIRST, LIST_EMPTY
      */
     
-    static struct Env *e;
-    if(--count <= 0 || curenv == NULL || curenv->env_status == ENV_NOT_RUNNABLE){
-        do {
+    if(--count <= 0 || curenv == NULL || e->env_status == ENV_NOT_RUNNABLE){
+        do { 
+        	e = LIST_FIRST(&env_sched_list[point]);
+            if (e == NULL) {
+                if (!LIST_EMPTY(&env_sched_list[1 - c_list])) {
+                    point = 1 - point;
+                    e = LIST_FIRST(&env_sched_list[c_list]);
+                } else
+                    panic("no runnable process\n");   
+            }
+            count = e->env_pri;
+            LIST_REMOVE(e, env_sched_link);
+            LIST_INSERT_HEAD(&env_sched_list[1 - point], e, env_sched_link);
         	if(LIST_EMPTY(&env_sched_list[point])) {
             	point = 1 - point;
        		}
-        	e = LIST_FIRST(&env_sched_list[point]);
-			if (e != NULL) {
-        		LIST_REMOVE(e, env_sched_link);
-        		LIST_INSERT_TAIL(&env_sched_list[1-point], e, env_sched_link);
-        		count = e->env_pri;
-			}
-    	} while(e == NULL || e->env_status != ENV_RUNNABLE);
+    	} while(e->env_status != ENV_RUNNABLE);
 	}
     env_run(e);
 }
